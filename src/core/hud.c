da3e3fb17471d1380198608c3cf0f9d85bbc0dc7
Charles the Thobe <charlesthethobe@gmail.com>
2026-09-18 16:36:57 +0300

    Better HUD warnings frequency and persist warning graphics for an extra tick

--- src/core/hud.c
+++ src/core/hud.c
@@ -104,18 +104,27 @@ void sr_hud_draw(sr_fb *fb, const sr_assets *a, const uint8_t *pristine, sr_play
 		draw_cells(fb, pristine, s->screen_ofs, s->w, s->h, s->cells,
 					0x5e, 0x5f, i < ful_segs);
 	}
+	const int warn_freq = 9;
 	/* out of oxygen */
 	if (p->end_state == 5) {
-		if (tick % 10 == 0) {
-			swap_hud_area_color(fb, pristine, 160, 161, 7, 6, 0x63, 0x64);
-			sfx(p, 3);
+		// Persist HUD warning for an extra adjacent tick
+		switch (tick % warn_freq) {
+			case 0:
+				sfx(p, 3);
+				[[fallthrough]];
+			case 1:
+				swap_hud_area_color(fb, pristine, 160, 161, 7, 6, 0x63, 0x64);
 		}
 	}
 	/* out of fuel */
 	if (p->end_state == 4) {
-		if (tick % 10 == 0) {
-			swap_hud_area_color(fb, pristine, 155, 169, 16, 4, 0x63, 0x64);
-			sfx(p, 3);
+		// Persist HUD warning for an extra adjacent tick
+		switch (tick % warn_freq) {
+			case 0:
+				sfx(p, 3);
+				[[fallthrough]];
+			case 1:
+				swap_hud_area_color(fb, pristine, 155, 169, 16, 4, 0x63, 0x64);
 		}
 	}
 	/* progress bar: 30 columns at x=42.., a slot is 6 pixels at its thickest */
